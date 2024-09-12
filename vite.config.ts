c5bbe457b9d2d86dd5d9d4b19acf77fbb5f93549
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2024-09-13 01:02:18 +0200

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+  };
+
+  if (command === "serve") {
+    // Development configuration
+    return {
+      ...config,
+    };
+  } else {
+    // Production configuration
+    return {
+      ...config,
+      build: {
+        lib: {
+          entry: "./src/main.tsx",
+          name: "InjectiveLiteBridge",
+          fileName: (format) => `index.${format}.js`,
+          formats: ["umd", "es"],
+        },
+        rollupOptions: {
+          external: ["react", "react-dom"],
+          output: {
+            globals: {
+              react: "React",
+              "react-dom": "ReactDOM",
+            },
+          },
+        },
+      },
+    };
+  }
+});
