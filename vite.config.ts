8fca634b96411fae00746af11e9e19d208f1ba49
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2024-09-18 14:35:50 +0200

    chore: testnet build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { nodePolyfills } from "@bangjelkoski/vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
   const config = {
     plugins: [react(), nodePolyfills({ protocolImports: true })],
   };
@@ -15,13 +15,19 @@ export default defineConfig(({ command }) => {
     };
   } else {
     // Production configuration
+
+    const isTestnet = mode === "testnet";
+    const prefix = isTestnet ? ".testnet" : "";
+    const outDir = isTestnet ? "dist/testnet" : "dist/mainnet";
+
     return {
       ...config,
       build: {
+        outDir: outDir,
         lib: {
           entry: "./src/main.tsx",
           name: "InjectiveLiteBridge",
-          fileName: (format) => `index.${format}.js`,
+          fileName: (format) => `index${prefix}.${format}.js`,
           formats: ["umd", "es"],
         },
         rollupOptions: {
